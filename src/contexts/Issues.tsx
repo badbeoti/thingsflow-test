f63d1525ade2908c34782fa0211b8f32f8f78a31
badbeoti <crab4251@gmail.com>
2022-10-04 18:04:48 +0900

    feat: issueDetail state 추가

--- src/contexts/Issues.tsx
+++ src/contexts/Issues.tsx
@@ -13,9 +13,11 @@ export type Issue = {
 };
 
 export const IssuesContext: React.Context<{
+  issueDetail: Issue;
   issues: Issue[];
   setIssues: (issues: Issue[]) => void;
 }> = createContext({
+  issueDetail: {} as Issue,
   issues: [] as Issue[],
   setIssues: (issues: Issue[]) => {},
 });
@@ -23,11 +25,18 @@ export const IssuesContext: React.Context<{
 export const IssuesProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
+  const [issueDetail, setIssueDetail] = React.useState<Issue>({});
   const [issues, setIssues] = React.useState<Issue[]>([]);
+  const handleSetIssueDetail = (issue: Issue) => setIssueDetail(issue);
   const handleSetIssues = (issues: Issue[]) => setIssues(issues);
 
   const value = React.useMemo(
-    () => ({ issues, setIssues: handleSetIssues }),
+    () => ({
+      issues,
+      setIssues: handleSetIssues,
+      issueDetail,
+      setIssueDetail: handleSetIssueDetail,
+    }),
     [issues, handleSetIssues],
   );
 
